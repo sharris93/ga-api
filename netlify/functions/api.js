9c9b27ed662e06624155d082439072df383cb233
Sam Harris <sharrisdev@gmail.com>
2024-04-09 22:55:27 +0100

    added basic express app

---
+++ netlify/functions/api.js
@@ -0,0 +1,95 @@
+import express, { Router } from 'express'
+import serverless from 'serverless-http'
+
+const app = express()
+
+const router = Router()
+
+// Routes
+router.get('/birds', (req, res) => res.json([
+  { 
+    species: 'Blue Tit',
+    subspecies: 'tit',
+    habitat: 'Woodlands and gardens',
+    color: 'Blue, yellow, white',
+    size: 'Small',
+    diet: 'Insects, seeds, nuts'
+  },
+  {
+    species: 'Great Tit',
+    subspecies: 'tit',
+    habitat: 'Woodlands, parks, gardens',
+    color: 'Black, white, yellow',
+    size: 'Medium',
+    diet: 'Insects, seeds, fruits'
+  },
+  {
+    species: 'American Robin',
+    subspecies: 'robin',
+    habitat: 'Forests, gardens, parks',
+    color: 'Brown, orange, gray',
+    size: 'Medium',
+    diet: 'Insects, worms, fruits'
+  },
+  {
+    species: 'European Robin',
+    subspecies: 'robin',
+    habitat: 'Woodlands, parks, gardens',
+    color: 'Reddish-brown, white',
+    size: 'Small',
+    diet: 'Insects, berries, seeds'
+  },
+  {
+    species: 'Bald Eagle',
+    subspecies: 'eagle',
+    habitat: 'Forests, near water',
+    color: 'Dark brown, white',
+    size: 'Large',
+    diet: 'Fish, mammals, birds'
+  },
+  {
+    species: 'Golden Eagle',
+    subspecies: 'eagle',
+    habitat: 'Mountainous regions',
+    color: 'Dark brown, gold',
+    size: 'Very large',
+    diet: 'Small mammals, birds'
+  },
+  {
+    species: 'Mallard Duck',
+    subspecies: 'duck',
+    habitat: 'Lakes, ponds, rivers',
+    color: 'Mottled brown, iridescent blue',
+    size: 'Medium',
+    diet: 'Aquatic plants, insects'
+  },
+  {
+    species: 'Wood Duck',
+    subspecies: 'duck',
+    habitat: 'Forested wetlands, marshes',
+    color: 'Colorful plumage with iridescent hues',
+    size: 'Medium',
+    diet: 'Aquatic plants, insects, seeds'
+  },
+  {
+    species: 'Peregrine Falcon',
+    subspecies: 'falcon',
+    habitat: 'Cliffs, urban areas',
+    color: 'Dark blue-gray, white',
+    size: 'Medium',
+    diet: 'Birds (often caught in flight)'
+  },
+  {
+    species: 'Gyrfalcon',
+    subspecies: 'falcon',
+    habitat: 'Arctic regions, tundra',
+    color: 'White, gray, black',
+    size: 'Large',
+    diet: 'Birds, small mammals'
+  }
+]))
+
+
+app.use('/api/', router)
+
+export const handler = serverless(app)
