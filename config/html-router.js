b867ebcb408606e53d654399e60af937fb15b724
Sam <sharrisdev@gmail.com>
2025-04-28 11:24:42 +0100

    added birds documentation

--- config/html-router.js
+++ config/html-router.js
@@ -3,11 +3,15 @@ import express from 'express'
 const router = express.Router()
 
 router.get('/events', (req, res) => {
-  return res.render('index.ejs')
+  return res.render('events.ejs')
 })
 
 router.get('/movies', (req, res) => {
   return res.render('movies.ejs')
+})
+
+router.get('/birds', (req, res) => {
+  return res.render('birds.ejs')
 })
 
 
---
+++ db/data/lotr-characters.js
@@ -0,0 +1,72 @@
+export default [
+  {
+    name: "Frodo Baggins", 
+    race: "Hobbit", 
+    role: "Ring-bearer", 
+    image: "https://static.wikia.nocookie.net/lotr/images/3/32/Frodo_%28FotR%29.png/revision/latest?cb=20221006065757", 
+    films: ["The Fellowship of the Ring", "The Two Towers", "The Return of the King"]
+  },
+  {
+    name: "Gandalf", 
+    race: "Maia", 
+    role: "Wizard", 
+    image: "https://static.wikia.nocookie.net/lotr/images/e/e7/Gandalf_the_Grey.jpg/revision/latest?cb=20121110131754", 
+    films: ["The Fellowship of the Ring", "The Two Towers", "The Return of the King"]
+  },
+  {
+    name: "Aragorn", 
+    race: "Human", 
+    role: "King of Gondor", 
+    image: "", 
+    films: ["The Fellowship of the Ring", "The Two Towers", "The Return of the King"]
+  },
+  {
+    name: "Legolas", 
+    race: "Elf", 
+    role: "Prince of the Woodland Realm", 
+    image: "", 
+    films: ["The Fellowship of the Ring", "The Two Towers", "The Return of the King"]
+  },
+  {
+    name: "Gimli", 
+    race: "Dwarf", 
+    role: "Warrior", 
+    image: "", 
+    films: ["The Fellowship of the Ring", "The Two Towers", "The Return of the King"]
+  },
+  {
+    name: "Samwise Gamgee", 
+    race: "Hobbit", 
+    role: "Frodo's Companion", 
+    image: "", 
+    films: ["The Fellowship of the Ring", "The Two Towers", "The Return of the King"]
+  },
+  {
+    name: "Boromir", 
+    race: "Human", 
+    role: "Son of Denethor II", 
+    image: "", 
+    films: ["The Fellowship of the Ring"]
+  },
+  {
+    name: "Galadriel", 
+    race: "Elf", 
+    role: "Lady of Lothlórien", 
+    image: "", 
+    films: ["The Fellowship of the Ring", "The Two Towers", "The Return of the King"]
+  },
+  {
+    name: "Saruman", 
+    race: "Maia", 
+    role: "Corrupted Wizard", 
+    image: "", 
+    films: ["The Fellowship of the Ring", "The Two Towers", "The Return of the King"]
+  },
+  {
+    name: "Gollum", 
+    race: "Hobbit-kind", 
+    role: "Ring-addict", 
+    image: "", 
+    films: ["The Fellowship of the Ring", "The Two Towers", "The Return of the King"]
+  }
+]
