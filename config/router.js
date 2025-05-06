a9816d86449d14a37294e12789929c2b484136c6
Sam <sharrisdev@gmail.com>
2025-05-06 17:57:17 +0100

    added auth-less CRUD for activities

--- config/router.js
+++ config/router.js
@@ -2,7 +2,7 @@ import express from 'express'
 import { eventIndex, eventCreate, eventShow, eventEdit, eventDelete } from '../controllers/events.js'
 import { movieIndex, movieShow, movieSearch, movieSuggestions } from '../controllers/movies.js'
 import { birdIndex, birdShow } from '../controllers/birds.js'
-import { activityIndex, activityShow } from '../controllers/activities.js'
+import { activityIndex, activityShow, activityCreate, activityUpdate, activityDelete } from '../controllers/activities.js'
 import secureRoute from '../lib/secureRoute.js'
 import auth from '../controllers/auth.js'
 
@@ -39,9 +39,12 @@ router.route('/birds/:id')
 
 router.route('/activities')
   .get(activityIndex)
+  .post(activityCreate)
 
 router.route('/activities/:activityId')
   .get(activityShow)
+  .put(activityUpdate)
+  .delete(activityDelete)
 
 router.post('/register', auth.register)
 router.post('/login', auth.login)
--- controllers/activities.js
+++ controllers/activities.js
@@ -5,7 +5,16 @@ import { Activity } from '../models/Trips/trip.js'
 export async function activityIndex(_req, res, next) {
   try {
     const activities = await Activity.find()
-    return res.status(200).json(activities)
+    return res.json(activities)
+  } catch (err) {
+    next(err)
+  }
+}
+
+export async function activityCreate(req, res, next) {
+  try {
+    const newActivity = await Activity.create(req.body)
+    return res.json(newActivity)
   } catch (err) {
     next(err)
   }
@@ -19,6 +28,34 @@ export async function activityShow(req, res, next) {
       throw new NotFound()
     }
     return res.status(200).json(foundActivity)
+  } catch (err) {
+    next(err)
+  }
+}
+
+export async function activityUpdate(req, res, next) {
+  const { activityId } = req.params
+  try {
+    const foundActivity = await Activity.findById(activityId)
+    if (!foundActivity) {
+      throw new NotFound()
+    }
+    const updatedActivity = await Activity.findByIdAndUpdate(activityId, req.body, { new: true })
+    return res.status(200).json(updatedActivity)
+  } catch (err) {
+    next(err)
+  }
+}
+
+export async function activityDelete(req, res, next) {
+  const { activityId } = req.params
+  try {
+    const foundActivity = await Activity.findById(activityId)
+    if (!foundActivity) {
+      throw new NotFound()
+    }
+    await Activity.findByIdAndDelete(activityId)
+    return res.sendStatus(204)
   } catch (err) {
     next(err)
   }
